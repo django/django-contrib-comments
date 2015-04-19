b143dfee9c528cd4fed01554b400b6afeab2fc29
José L. Patiño <jose.lpa@gmail.com>
2015-04-19 12:44:06 +0100

    Fixed deprecated `GenericForeignKey` import. Now defined in `django.contrib.contenttypes.fields`.

--- django_comments/models.py
+++ django_comments/models.py
@@ -1,12 +1,12 @@
 from django.conf import settings
-from django.contrib.contenttypes import generic
+from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.sites.models import Site
 from django.core import urlresolvers
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
 from django.utils import timezone
 from django.utils.encoding import python_2_unicode_compatible
+from django.utils.translation import ugettext_lazy as _
 
 from django_comments.managers import CommentManager
 
@@ -20,11 +20,12 @@ class BaseCommentAbstractModel(models.Model):
     """
 
     # Content-object field
-    content_type = models.ForeignKey(ContentType,
-            verbose_name=_('content type'),
-            related_name="content_type_set_for_%(class)s")
+    content_type = models.ForeignKey(
+        ContentType, verbose_name=_('content type'),
+        related_name="content_type_set_for_%(class)s")
     object_pk = models.TextField(_('object ID'))
-    content_object = generic.GenericForeignKey(ct_field="content_type", fk_field="object_pk")
+    content_object = GenericForeignKey(
+        ct_field="content_type", fk_field="object_pk")
 
     # Metadata about the comment
     site = models.ForeignKey(Site)
