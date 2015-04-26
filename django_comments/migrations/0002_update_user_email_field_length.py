5d565503b26f71fd8ccecd67c796812914cddbad
José L. Patiño <jose.lpa@gmail.com>
2015-04-26 13:46:23 +0100

    Migrated user_email to 254 chars
    EmailField defaults to 254 chars in Django 1.8. This
    migration applies that both for 1.7 and 1.8.

---
+++ django_comments/migrations/0002_update_user_email_field_length.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('django_comments', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='comment',
+            name='user_email',
+            field=models.EmailField(
+                max_length=254, verbose_name="user's email address",
+                blank=True),
+        ),
+    ]
