a984fbc933ffccbf0c7296bd2d2f61a41b8e6bf8
José L. Patiño <jose.lpa@gmail.com>
2015-04-27 13:13:45 +0100

    Fixed `Comments.user_email` field to a max length of 254 chars.

--- django_comments/models.py
+++ django_comments/models.py
@@ -59,7 +59,8 @@ class Comment(BaseCommentAbstractModel):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_('user'),
                              blank=True, null=True, related_name="%(class)s_comments")
     user_name = models.CharField(_("user's name"), max_length=50, blank=True)
-    user_email = models.EmailField(_("user's email address"), blank=True)
+    user_email = models.EmailField(_("user's email address"), max_length=254,
+                                   blank=True)
     user_url = models.URLField(_("user's URL"), blank=True)
 
     comment = models.TextField(_('comment'), max_length=COMMENT_MAX_LENGTH)
