5737956ac7269781b4c6f890579705e26a49996b
José L. Patiño <jose.lpa@gmail.com>
2015-04-27 13:20:54 +0100

    Fixed migration 0002.

--- django_comments/migrations/0002_update_user_email_field_length.py
+++ django_comments/migrations/0002_update_user_email_field_length.py
@@ -13,17 +13,6 @@ class Migration(migrations.Migration):
     operations = [
         migrations.AlterField(
             model_name='comment',
-            name='is_removed',
-            field=models.BooleanField(
-                default=False,
-                help_text='Check this box if the comment is inappropriate. '
-                          'A "This comment has been removed" message will be '
-                          'displayed instead.',
-                verbose_name='is removed'
-            ),
-        ),
-        migrations.AlterField(
-            model_name='comment',
             name='user_email',
             field=models.EmailField(
                 max_length=254, verbose_name="user's email address",
