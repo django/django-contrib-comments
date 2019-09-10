3cbc9332d5d78accad955ce2e33c13f0edc1d72c
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2019-09-10 11:10:29 +0200

    Used python_2_unicode_compatible from six.
    django.utils.encoding.python_2_unicode_compatible() alias was removed
    in Django 3.0.

--- django_comments/abstracts.py
+++ django_comments/abstracts.py
@@ -7,8 +7,8 @@ from django.contrib.sites.models import Site
 from django.db import models
 from django.urls import reverse
 from django.utils import timezone
-from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
+from six import python_2_unicode_compatible
 
 from .managers import CommentManager
 
--- django_comments/models.py
+++ django_comments/models.py
@@ -1,8 +1,8 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
-from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
+from six import python_2_unicode_compatible
 
 from .abstracts import (
     COMMENT_MAX_LENGTH, BaseCommentAbstractModel, CommentAbstractModel,
