2425ea9b2eb1212b0979ad261e4e16e5e0b6bca7
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2020-05-23 15:40:42 +0200

    Prevented is_safe_url() deprecation warning.

--- django_comments/views/utils.py
+++ django_comments/views/utils.py
@@ -8,7 +8,8 @@ from urllib.parse import urlencode
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, resolve_url
 from django.core.exceptions import ObjectDoesNotExist
-from django.utils.http import is_safe_url
+
+from ..compat import url_has_allowed_host_and_scheme
 
 import django_comments
 
@@ -24,7 +25,7 @@ def next_redirect(request, fallback, **get_kwargs):
     Returns an ``HttpResponseRedirect``.
     """
     next = request.POST.get('next')
-    if not is_safe_url(url=next, allowed_hosts={request.get_host()}):
+    if not url_has_allowed_host_and_scheme(url=next, allowed_hosts={request.get_host()}):
         next = resolve_url(fallback)
 
     if get_kwargs:
