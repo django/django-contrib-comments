126fc12beb87f598c3ee803fd03036ad410e92cb
Mr. Senko <atodorov@mrsenko.com>
2020-08-20 00:02:49 +0300

    Remove Signal(providing_args) argument b/c it is deprecated
    RemovedInDjango40Warning: The providing_args argument is deprecated.
    As it is purely documentational, it has no replacement. If you rely
    on this argument as documentation, you can move the text to a code
    comment or docstring.

--- django_comments/signals.py
+++ django_comments/signals.py
@@ -9,13 +9,16 @@ from django.dispatch import Signal
 # discarded and a 400 response. This signal is sent at more or less
 # the same time (just before, actually) as the Comment object's pre-save signal,
 # except that the HTTP request is sent along with this signal.
-comment_will_be_posted = Signal(providing_args=["comment", "request"])
+# Arguments: "comment", "request"
+comment_will_be_posted = Signal()
 
 # Sent just after a comment was posted. See above for how this differs
 # from the Comment object's post-save signal.
-comment_was_posted = Signal(providing_args=["comment", "request"])
+# Arguments: "comment", "request"
+comment_was_posted = Signal()
 
 # Sent after a comment was "flagged" in some way. Check the flag to see if this
 # was a user requesting removal of a comment, a moderator approving/removing a
 # comment, or some other custom user flag.
-comment_was_flagged = Signal(providing_args=["comment", "flag", "created", "request"])
+# Arguments: "comment", "flag", "created", "request"
+comment_was_flagged = Signal()
