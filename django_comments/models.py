0ae724b2544ff197a21044a7386a0156da88258d
Frantisek Holop <fholop@ripe.net>
2020-10-21 09:58:22 +0200

    remove unused imports

--- django_comments/models.py
+++ django_comments/models.py
@@ -3,9 +3,7 @@ from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
-from .abstracts import (
-    COMMENT_MAX_LENGTH, BaseCommentAbstractModel, CommentAbstractModel,
-)
+from .abstracts import CommentAbstractModel
 
 
 class Comment(CommentAbstractModel):
